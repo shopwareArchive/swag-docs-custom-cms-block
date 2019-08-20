42dac9a81eab97d794a9a950d9196c9b078955e8
Stephan Pohl <stp@shopware.com>
2019-08-20 13:26:28 +0200

    Use third party interface

--- src/Resources/administration/module/sw-cms/blocks/text-image/image-text-reversed/preview/index.js
+++ src/Resources/administration/module/sw-cms/blocks/text-image/image-text-reversed/preview/index.js
@@ -1,6 +1,7 @@
-import { Component } from 'src/core/shopware';
 import template from './sw-cms-preview-image-text-reversed.html.twig';
 import './sw-cms-preview-image-text-reversed.scss';
+
+const { Component } = Shopware;
 
 Component.register('sw-cms-preview-image-text-reversed', {
     template
