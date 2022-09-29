8db4094a8c36f614bb46dec16d5762469ce28261
bendixcrse <35821642+bendixcrse@users.noreply.github.com>
2022-09-29 14:59:55 +0200

    Update index.js

--- src/Resources/app/administration/src/module/sw-cms/blocks/text-image/image-text-reversed/component/index.js
+++ src/Resources/app/administration/src/module/sw-cms/blocks/text-image/image-text-reversed/component/index.js
@@ -1,7 +1,6 @@
-import { Component } from 'src/core/shopware';
 import template from './sw-cms-block-image-text-reversed.html.twig';
 import './sw-cms-block-image-text-reversed.scss';
 
-Component.register('sw-cms-block-image-text-reversed', {
+Shopware.Component.register('sw-cms-block-image-text-reversed', {
     template
 });
